a35ba80d1bcf8f2d6fd21ee3514b45d54c88f8fe
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-17 11:33:24 +0700

    7. Laravel Fundamentals - Laravel Blade templating engine: 2. Master layout setup

--- cms/resources/views/contact.blade.php
+++ cms/resources/views/contact.blade.php
@@ -1,16 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Laravel</title>
+@extends('layouts.app')
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-</head>
-<body>
-<div class="container">
-
+@section('content')
     <h1>Contact Page</h1>
+@endsection
 
-</div>
-</body>
-</html>
+@section('footer')
+    <script>alert('Hello World!!')</script>
+
+@endsection
