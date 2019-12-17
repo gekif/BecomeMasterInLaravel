967229024235af98b923fc2a6729aa2bdb7c0440
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-17 12:24:52 +0700

    10. Laravel Fundamentals - Database - Eloquent ORM: 2. Reading Data

--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -2,83 +2,31 @@
 
 /*
 |--------------------------------------------------------------------------
-| Application Routes
+| Laravel Eloquent/ORM
 |--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-//Route::resource('posts', 'PostsController');
-
-
-//Route::get('/contact', 'PostsController@contact');
-
-
-//Route::get('post/{id}/{name}/{password}', 'PostsController@showPost');
-
-
-/*
-|--------------------------------------------------------------------------
-| Database RAW SQL Queries
-|--------------------------------------------------------------------------
-|
 */
 
 
-// Insert
-/*Route::get('/insert', function () {
+// Reading Data
+use App\Post;
 
-    DB::insert('insert into posts(title, body) values(?, ?)', [
-        'PHP With Laravel',
-        'Larave is the best thing that has happened to PHP'
-    ]);
+Route::get('/read', function () {
 
-});*/
+    $posts = Post::all();
 
-
-// Read
-/*Route::get('/read', function () {
-
-    $results = DB::select('select * from posts where id = ?', [1]);
-
-//    foreach ($results as $post) {
-//        return $post->title;
-//    }
-
-    // Return JSON
-    return $results;
-
-    // debugging
-//     dd($results);
-//     var_dump($results);
-
-});*/
-
-
-// Update
-/*Route::get('/update', function () {
-
-    $updated = DB::update('update posts set title = ? where id = ?', [
-                    'PHP With Laravel Updated',
-                    2
-                ]);
-
-
-    // Return 1 -> true
-    // Return 0 -> false
-    return $updated;
-
-});*/
-
-
-// Delete
-Route::get('/delete', function () {
-
-    $deleted = DB::delete('delete from posts where id = ?', [1]);
-
-    return $deleted;
+    foreach ($posts as $post) {
+        return $post->title;
+    }
 
 });
+
+
+Route::get('/find', function () {
+
+    $posts = Post::find(1);
+
+    return $posts->title;
+
+
+});
+
