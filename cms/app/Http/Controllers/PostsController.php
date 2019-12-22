7959bbe789986a857a7977ad25fda60bde1eaa0f
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-22 20:35:50 +0700

    19. Forms - Package and validation: 7. Advance validation

--- cms/app/Http/Controllers/PostsController.php
+++ cms/app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreatePostRequest;
 use App\Post;
 use Illuminate\Http\Request;
 
@@ -38,11 +39,11 @@ class PostsController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(CreatePostRequest $request)
     {
-        $this->validate($request, [
-            'title' => 'required'
-        ]);
+//        $this->validate($request, [
+//            'title' => 'required'
+//        ]);
 
         Post::create($request->all());
 
