18ff7f41ae19e2fbfbcc96cdfe06d358a1473ddf
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2019-12-23 16:01:57 +0700

    20. Database - Some more model manipulation: 2. Dates

--- cms/app/Http/routes.php
+++ cms/app/Http/routes.php
@@ -6,6 +6,44 @@
 
     Route::resource('/posts', 'PostsController');
 
+    Route::get('/dates', function () {
+
+        $date = new DateTime('+1 week');
+
+        echo $date->format('m-d-Y');
+
+    });
+
 });*/
 
+use Carbon\Carbon;
+
 Route::resource('/posts', 'PostsController');
+
+
+Route::get('/dates', function () {
+
+    $date = new DateTime('+1 week');
+
+    echo $date->format('m-d-Y');
+    echo '<br>';
+
+    echo Carbon::now();
+    echo '<br>';
+
+    echo Carbon::now()->diffForHumans();
+    echo '<br>';
+
+    echo Carbon::now()->addDays(10)->diffForHumans();
+    echo '<br>';
+
+    echo Carbon::now()->subMonths(5)->diffForHumans();
+    echo '<br>';
+
+    echo Carbon::now()->yesterday()->diffForHumans();
+    echo '<br>';
+
+
+
+
+});
