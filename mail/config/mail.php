9b8407cfd30a8cc86355a791ea7e311626572eae
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-02 20:48:05 +0700

    25. Laravel - Sending Email  Api: 5. Configuration

--- mail/config/mail.php
+++ mail/config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'dzulfikar.maulana@gmail.com', 'name' => 'Dzulfikar Maulana'],
 
     /*
     |--------------------------------------------------------------------------
