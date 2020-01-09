32593b56e71c740961f2781aec4f13d4a489e86e
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-09 16:31:23 +0700

    27. Application: 28. Displaying photos using an accessor

--- codehacking/resources/views/admin/users/index.blade.php
+++ codehacking/resources/views/admin/users/index.blade.php
@@ -8,6 +8,7 @@
         <thead>
           <tr>
             <th>Id</th>
+            <th>Photo</th>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
@@ -23,6 +24,7 @@
         @foreach($users as $user)
           <tr>
             <td>{{ $user->id }}</td>
+            <td><img height="50" src="{{ $user->photo ? $user->photo->file : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png' }}" alt=""></td>
             <td>{{ $user->name }}</td>
             <td>{{ $user->email }}</td>
             <td>{{ $user->role->name }}</td>
