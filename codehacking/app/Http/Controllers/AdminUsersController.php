1750aa645c4f2988b463f66fefacb34890ff5ff6
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-09 16:14:32 +0700

    27. Application: 26. Persisting user with photo part 1

--- codehacking/app/Http/Controllers/AdminUsersController.php
+++ codehacking/app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\Role;
 use App\User;
 use Illuminate\Http\Request;
@@ -44,10 +45,18 @@ class AdminUsersController extends Controller
      */
     public function store(UsersRequest $request)
     {
-//        $input = $request->all();
+        $input = $request->all();
 
-        if ($request->file('photo_id')) {
-            return "photo exist";
+        if ($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+
         }
 
 //        return redirect('/admin/users');
