3ca71dd764a9a401267e0243958b8621edae32c9
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-09 16:48:24 +0700

    27. Application: 30. Edit user part 2 - displaying images and status

--- codehacking/resources/views/admin/users/edit.blade.php
+++ codehacking/resources/views/admin/users/edit.blade.php
@@ -3,82 +3,88 @@
 @section('content')
 
     <h1>Edit Users</h1>
+    
+    <div class="col-sm-3">
+        <img src="{{$user->photo ? $user->photo->file : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png'}}" alt="" class="img-responsive img-rounded">
+    </div>
 
-    {!! Form::model($user, [
+    <div class="col-sm-9">
+        {!! Form::model($user, [
             'method' => 'PATCH',
             'action' => ['AdminUsersController@edit', $user->id],
             'files' => true
         ]) !!}
 
-    <div class="form-group">
+        <div class="form-group">
 
-        {!! Form::label('name', 'Name:') !!}
+            {!! Form::label('name', 'Name:') !!}
 
-        {!! Form::text('name', null,[
-            'class' => 'form-control'
-        ]) !!}
+            {!! Form::text('name', null,[
+                'class' => 'form-control'
+            ]) !!}
 
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('email', 'Email:') !!}
+
+            {!! Form::email('email', null,[
+                'class' => 'form-control'
+            ]) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('role_id', 'Role:') !!}
+
+            {!! Form::select('role_id', $roles , null,[
+                'class' => 'form-control'
+            ]) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('is_active', 'Status:') !!}
+
+            {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), null,[
+                'class' => 'form-control'
+            ]) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('photo_id', 'File:') !!}
+
+            {!! Form::file('photo_id', null,[
+                'class' => 'form-control'
+            ]) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('password', 'Password:') !!}
+
+            {!! Form::password('password',[
+                'class' => 'form-control'
+            ]) !!}
+
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::submit('Edit User', [
+                    'class' => 'btn btn-primary'
+            ]) !!}
+
+        </div>
+
+        {!! Form::close() !!}
     </div>
-
-    <div class="form-group">
-
-        {!! Form::label('email', 'Email:') !!}
-
-        {!! Form::email('email', null,[
-            'class' => 'form-control'
-        ]) !!}
-
-    </div>
-
-    <div class="form-group">
-
-        {!! Form::label('role_id', 'Role:') !!}
-
-        {!! Form::select('role_id', $roles , null,[
-            'class' => 'form-control'
-        ]) !!}
-
-    </div>
-
-    <div class="form-group">
-
-        {!! Form::label('is_active', 'Status:') !!}
-
-        {!! Form::select('is_active', array(1 => 'Active', 0 => 'Not Active'), null,[
-            'class' => 'form-control'
-        ]) !!}
-
-    </div>
-
-    <div class="form-group">
-
-        {!! Form::label('photo_id', 'File:') !!}
-
-        {!! Form::file('photo_id', null,[
-            'class' => 'form-control'
-        ]) !!}
-
-    </div>
-
-    <div class="form-group">
-
-        {!! Form::label('password', 'Password:') !!}
-
-        {!! Form::password('password',[
-            'class' => 'form-control'
-        ]) !!}
-
-    </div>
-
-    <div class="form-group">
-
-        {!! Form::submit('Edit User', [
-                'class' => 'btn btn-primary'
-        ]) !!}
-
-    </div>
-
-    {!! Form::close() !!}
 
     @include('includes.form_error')
 
