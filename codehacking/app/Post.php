22730b177ae0ad61b0f09699a432d48466f17c5d
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-13 14:56:21 +0700

    28. Application - Posts: 3. Displaying post

--- codehacking/app/Post.php
+++ codehacking/app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+      'user_id',
+      'category_id',
+      'photo_id',
+      'title',
+      'body'
+    ];
 }
