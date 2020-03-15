8c448786e44c67e171d62b8f17aff79a9c1cfae1
Gekif <dzulfikar.maulana@gmail.com>
2020-03-15 20:16:01 +0700

    31. Application - Comments: 1. Setting up part 1

--- codehacking/app/Http/routes.php
+++ codehacking/app/Http/routes.php
@@ -37,5 +37,9 @@ Route::group(['middlware' => 'admin'], function () {
 
     Route::resource('admin/media', 'AdminMediasController');
 
+    Route::resource('admin/comments', 'PostCommentsController');
+
+    Route::resource('admin/comment/replies', 'CommentRepliesController');
+
 });
 
