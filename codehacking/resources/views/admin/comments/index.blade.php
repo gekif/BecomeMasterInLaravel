cd44334e988c58576869104c0162e3698aef291a
Gekif <dzulfikar.maulana@gmail.com>
2020-03-26 21:28:43 +0700

    31. Application - Comments: 8. Displaying posts

--- codehacking/resources/views/admin/comments/index.blade.php
+++ codehacking/resources/views/admin/comments/index.blade.php
@@ -2,6 +2,41 @@
 
 @section('content')
 
-    <h1>Comments</h1>
+
+@if (count($comments) > 0)
+
+<h1>Comments</h1>
+
+<table class="table">
+    <thead>
+    <tr>
+        <th>No</th>
+        <th>Author</th>
+        <th>Email</th>
+        <th>Body</th>
+        <th>Detail</th>
+    </tr>
+    </thead>
+    <tbody>
+
+    @php $i = 1 @endphp
+    @foreach ($comments as $comment)
+    <tr>
+        <td>{{ $i++ }}</td>
+        <td>{{ $comment->author }}</td>
+        <td>{{ $comment->email }}</td>
+        <td>{{ $comment->body }}</td>
+        <td><a href="{{ route('home.post', $comment->post->id) }}">View Post </a></td>
+    </tr>
+    @endforeach
+
+    </tbody>
+</table>
+
+@else
+    <h1 class="text-center">No Comments</h1>
+
+
+@endif
 
 @stop
