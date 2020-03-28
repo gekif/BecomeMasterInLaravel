6b53f4fe437664d012e4795a706aa00f704045be
Gekif <dzulfikar.maulana@gmail.com>
2020-03-28 16:44:59 +0700

    35. Extra feature - Disqus Comment System

--- codehacking/resources/views/post.blade.php
+++ codehacking/resources/views/post.blade.php
@@ -34,119 +34,145 @@
 
     <hr>
 
+    <div id="disqus_thread"></div>
+    <script>
 
-    @if(Session::has('comment_message'))
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
+        var disqus_config = function () {
+        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+        };
+        */
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://gekif-codehacking.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
+    </script>
 
-            {{session('comment_message')}}
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+
+    <script id="dsq-count-scr" src="//gekif-codehacking.disqus.com/count.js" async></script>
 
 
-       @endif
+    {{--@if(Session::has('comment_message'))--}}
+
+            {{--{{session('comment_message')}}--}}
+
+
+       {{--@endif--}}
 
     <!-- Blog Comments -->
 
 
-@if(Auth::check())
+{{--@if(Auth::check())--}}
 
-    <!-- Comments Form -->
-    <div class="well">
-        <h4>Leave a Comment:</h4>
-
-
-        {!! Form::open(['method'=>'POST', 'action'=> 'PostCommentsController@store']) !!}
+    {{--<!-- Comments Form -->--}}
+    {{--<div class="well">--}}
+        {{--<h4>Leave a Comment:</h4>--}}
 
 
-              <input type="hidden" name="post_id" value="{{$post->id}}">
+        {{--{!! Form::open(['method'=>'POST', 'action'=> 'PostCommentsController@store']) !!}--}}
 
 
-             <div class="form-group">
-                 {!! Form::label('body', 'Body:') !!}
-                 {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>3])!!}
-             </div>
-
-             <div class="form-group">
-                 {!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}
-             </div>
-        {!! Form::close() !!}
+              {{--<input type="hidden" name="post_id" value="{{$post->id}}">--}}
 
 
-    </div>
+             {{--<div class="form-group">--}}
+                 {{--{!! Form::label('body', 'Body:') !!}--}}
+                 {{--{!! Form::textarea('body', null, ['class'=>'form-control','rows'=>3])!!}--}}
+             {{--</div>--}}
+
+             {{--<div class="form-group">--}}
+                 {{--{!! Form::submit('Submit comment', ['class'=>'btn btn-primary']) !!}--}}
+             {{--</div>--}}
+        {{--{!! Form::close() !!}--}}
 
 
-@endif
+    {{--</div>--}}
+
+
+{{--@endif--}}
 
     <hr>
+
+    {{-- Disqus Comment--}}
 
     <!-- Posted Comments -->
 
 
 
-@if(count($comments) > 0)
+{{--@if(count($comments) > 0)--}}
 
 
-        @foreach($comments as $comment)
+        {{--@foreach($comments as $comment)--}}
 
-    <!-- Comment -->
-    <div class="media">
-        <a class="pull-left" href="#">
-            <img height="64" class="media-object" src="{{Auth::user()->gravatar}}" alt="">
-        </a>
-        <div class="media-body">
-            <h4 class="media-heading">{{$comment->author}}
-                <small>{{$comment->created_at->diffForHumans()}}</small>
-            </h4>
-            <p>{{$comment->body}}</p>
+    {{--<!-- Comment -->--}}
+    {{--<div class="media">--}}
+        {{--<a class="pull-left" href="#">--}}
+            {{--<img height="64" class="media-object" src="{{Auth::user()->gravatar}}" alt="">--}}
+        {{--</a>--}}
+        {{--<div class="media-body">--}}
+            {{--<h4 class="media-heading">{{$comment->author}}--}}
+                {{--<small>{{$comment->created_at->diffForHumans()}}</small>--}}
+            {{--</h4>--}}
+            {{--<p>{{$comment->body}}</p>--}}
 
-            <div class="comment-reply-container">
-
-
-                <button class="toggle-reply btn btn-primary pull-right">Reply</button>
+            {{--<div class="comment-reply-container">--}}
 
 
-                <div class="comment-reply">
+                {{--<button class="toggle-reply btn btn-primary pull-right">Reply</button>--}}
 
 
-                    {!! Form::open(['method'=>'POST', 'action'=> 'CommentRepliesController@createReply']) !!}
-                    <div class="form-group">
-
-                        <input type="hidden" name="comment_id" value="{{$comment->id}}">
-
-                        {!! Form::label('body', 'Body:') !!}
-                        {!! Form::textarea('body', null, ['class'=>'form-control','rows'=>1])!!}
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::submit('submit', ['class'=>'btn btn-primary']) !!}
-                    </div>
-                    {!! Form::close() !!}
+                {{--<div class="comment-reply">--}}
 
 
-                </div>
+                    {{--{!! Form::open(['method'=>'POST', 'action'=> 'CommentRepliesController@createReply']) !!}--}}
+                    {{--<div class="form-group">--}}
 
-            </div>
+                        {{--<input type="hidden" name="comment_id" value="{{$comment->id}}">--}}
 
+                        {{--{!! Form::label('body', 'Body:') !!}--}}
+                        {{--{!! Form::textarea('body', null, ['class'=>'form-control','rows'=>1])!!}--}}
+                    {{--</div>--}}
 
-            @if(count($comment->replies) > 0)
-
-                  @foreach($comment->replies as $reply)
-
-                        @if($reply->is_active == 1)
-
-                        <!-- Nested Comment -->
-                        <div id="nested-comment" class=" media">
-                            <a class="pull-left" href="#">
-                                <img height="64" class="media-object" src="{{$reply->photo}}" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading"{{$reply->author}}
-                                    <small>{{$reply->created_at->diffForHumans()}}</small>
-                                </h4>
-                                <p>{{$reply->body}}</p>
-                            </div>
-
-            <!-- End Nested Comment -->
+                    {{--<div class="form-group">--}}
+                        {{--{!! Form::submit('submit', ['class'=>'btn btn-primary']) !!}--}}
+                    {{--</div>--}}
+                    {{--{!! Form::close() !!}--}}
 
 
-                    </div>
+                {{--</div>--}}
+
+            {{--</div>--}}
+
+
+            {{--@if(count($comment->replies) > 0)--}}
+
+                  {{--@foreach($comment->replies as $reply)--}}
+
+                        {{--@if($reply->is_active == 1)--}}
+
+                        {{--<!-- Nested Comment -->--}}
+                        {{--<div id="nested-comment" class=" media">--}}
+                            {{--<a class="pull-left" href="#">--}}
+                                {{--<img height="64" class="media-object" src="{{$reply->photo}}" alt="">--}}
+                            {{--</a>--}}
+                            {{--<div class="media-body">--}}
+                                {{--<h4 class="media-heading"{{$reply->author}}--}}
+                                    {{--<small>{{$reply->created_at->diffForHumans()}}</small>--}}
+                                {{--</h4>--}}
+                                {{--<p>{{$reply->body}}</p>--}}
+                            {{--</div>--}}
+
+            {{--<!-- End Nested Comment -->--}}
+
+
+                    {{--</div>--}}
 
                              {{--@else--}}
 
@@ -156,18 +182,18 @@
 
 
 
-                            @endif
+                            {{--@endif--}}
 
-                     @endforeach
+                     {{--@endforeach--}}
 
-            @endif
+            {{--@endif--}}
 
-        </div>
-    </div>
+        {{--</div>--}}
+    {{--</div>--}}
 
-     @endforeach
+     {{--@endforeach--}}
 
-@endif
+{{--@endif--}}
 
 @stop
 
