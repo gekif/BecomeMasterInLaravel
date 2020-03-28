fa8425e260c08e2cb6ce4aa5afeea37f837c350a
Gekif <dzulfikar.maulana@gmail.com>
2020-03-28 20:26:22 +0700

    39. Extra feature  Laravel data seeding: 3. Let's create factories for all our database tables

--- codehacking/database/seeds/DatabaseSeeder.php
+++ codehacking/database/seeds/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\User;
+use App\Post;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,8 +14,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class, 10)->create();
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
-//         $this->call(UsersTableSeeder::class);
+        DB::table('users')->truncate();
+        DB::table('posts')->truncate();
+        DB::table('roles')->truncate();
+        DB::table('categories')->truncate();
+        DB::table('photos')->truncate();
+        DB::table('comments')->truncate();
+        DB::table('comment_replies')->truncate();
+
+        factory(App\User::class, 10)->create()->each(function($user){
+            $user->posts()->save(factory(App\Post::class)
+                ->make()
+            );
+        });
+
+        factory(App\Role::class, 3)->create();
+
+        factory(App\Category::class, 10)->create();
+
+        factory(App\Photo::class, 1)->create();
+
+        factory(App\Comment::class, 10)->create()->each(function ($comment) {
+            $comment->replies()->save(factory(App\CommentReply::class)
+                ->make()
+            );
+        });
     }
 }
